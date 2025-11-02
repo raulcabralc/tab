1620a335e8ceb4615388a90fb94cb16f85c52187
Raul Cabral <raulcabralc@gmail.com>
2025-11-02 19:56:12 -0300

    chore: add class-validator to better validation in dtos

--- backend/src/order/types/dto/create-order.dto.ts
+++ backend/src/order/types/dto/create-order.dto.ts
@@ -5,27 +5,73 @@ import { OrderType } from "../enums/type.enum";
 import { OrderItem } from "../interfaces/item.interface";
 import { OrderPriority } from "../enums/priority.enum";
 import { OrderStatus } from "../enums/status.enum";
+import {
+  IsBoolean,
+  IsDate,
+  isNumber,
+  IsNumber,
+  IsString,
+} from "class-validator";
 
 export class CreateOrderDto {
+  @IsString()
   id?: string;
+
+  @IsString()
   priority: OrderPriority;
+
+  @IsNumber()
   number: number;
+
+  @IsString()
   status?: OrderStatus;
+
+  @IsDate()
   ordered: Date;
+
   items: OrderItem[];
+
+  @IsString()
   type: OrderType;
+
+  @IsNumber()
   tableNumber?: number;
+
   address?: Address;
+
+  @IsString()
   waiterId: string;
+
+  @IsNumber()
   subtotal: number;
+
+  @IsNumber()
   discount: number;
+
+  @IsNumber()
   deliveryFee: number;
+
+  @IsNumber()
   total: number;
+
+  @IsNumber()
   amountPaid: number;
+
+  @IsNumber()
   change?: number;
+
+  @IsString()
   paymentMethod: PaymentMethod;
+
+  @IsBoolean()
   isPaid?: boolean;
+
+  @IsDate()
   startedPreparing?: Date | null;
+
+  @IsDate()
   finishedPreparing?: Date | null;
+
+  @IsString()
   origin: Origin;
 }
---
+++ backend/src/worker/types/dto/create-worker.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsString } from "class-validator";
+import { WorkerRole } from "../enums/role.enum";
+
+export class CreateWorkerDto {
+  @IsString()
+  fullName: string;
+
+  @IsString()
+  displayName: string;
+
+  @IsString()
+  role: WorkerRole;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  pin: string;
+}
